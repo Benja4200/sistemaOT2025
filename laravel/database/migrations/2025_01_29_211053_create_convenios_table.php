055a13fc6920be4adf060dbf48f69d07853f2c52
3betclover <aldo.espinoza03@inacapmail.cl>
2025-06-04 10:40:40 -0400

    Modificacion de la migracion de convenios_table.php

--- laravel/database/migrations/2025_01_29_211053_create_convenios_table.php
+++ laravel/database/migrations/2025_01_29_211053_create_convenios_table.php
@@ -14,19 +14,16 @@ return new class extends Migration
         Schema::create('convenios', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_cliente')->constrained('cliente')->onDelete('cascade'); // Clave foránea para clientes
-            $table->foreignId('id_branch')->constrained()->onDelete('cascade'); // Clave foránea para sucursales
+            $table->foreignId('id_branch')->constrained('sucursal')->onDelete('cascade'); // Clave foránea para sucursales
             $table->integer('visitas_presenciales');
             $table->integer('visitas_emergencia');
             $table->integer('soporte_remoto');
             $table->integer('horas_tecnicas');
             $table->string('estado');
-            $table->timestamps(); // Incluye created_at y updated_at
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('convenios');
